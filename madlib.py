51cd5414f4cb55e6d7cebb4c5aac581c8731c367
inv0w <jonjoninfante@gmail.com>
2019-08-31 13:19:23 -0700

    Story implemented, some list frameworks

--- madlib.py
+++ madlib.py
@@ -1,1 +1,24 @@
-noun = input("Enter a noun")
+import os #For Clearing Lines
+nounlist = list() #Creates noun list
+adjlist = list() #Creates adjective list
+verblist = list() #Creates verb list
+propnoun = list() #Creates proper noun list
+
+def user_input(prompt):
+    response = input(prompt)
+    return response
+
+#def add(word):
+
+
+# story = f'''
+#     You find yourself lost and the first thing you see is a
+#     {nounlist[0]}. You consider touching the {nounlist[0]}
+#     however you {verblist[0]} away when you see it start to transform.
+#     The {adjlist[0]} object is morphing into a {nounlist[1]}
+#     , your worst fear. You run and {verblist[1]} but there is nowhere
+#     to go in the void you find yourself in.  The last thing you see before
+#     it consumes you is a {nounlist[2]}. You are relieved now.'''
+#
+#
+# print(story)
